8b5b87bae15249ee5df50301e9eeafe1be47a58c
Yohei <yo_kubocchi@hotmail.com>
2018-03-13 12:44:01 -0700

    New Challenge

---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 122px;
+            height: 150px;
+        }
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: normal;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="" class="auto-style1" src="epic-spies-logo.jpg" /><br />
+            <h2><span class="auto-style2"><strong>Asset Performance Tracker</strong></span><br />
+            </h2>
+            Asset Name: <asp:TextBox ID="assetNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Elections Rigged: <asp:TextBox ID="electionsTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="actsSubterfugeTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addAssetButton" runat="server" OnClick="addAssetButton_Click" Text="Add Asset" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] assets = new string[0];
+                ViewState.Add("Assets", assets);
+                double[] elections = new double[0];
+                ViewState.Add("Elections", elections);
+                double[] acts = new double[0];
+                ViewState.Add("Acts", acts);
+                
+            }
+
+        }
+
+        protected void addAssetButton_Click(object sender, EventArgs e)
+        {
+            //Get previous data from ViewState to variable, and resize variable(length +1)
+            string[] assets = (string[])ViewState["Assets"];
+            Array.Resize(ref assets, assets.Length + 1);
+            double[] elections = (double[])ViewState["Elections"];
+            Array.Resize(ref elections, elections.Length + 1);
+            double[] acts = (double[])ViewState["Acts"];
+            Array.Resize(ref acts, acts.Length + 1);
+
+            //Get input data from TextBoxes
+            assets[assets.GetUpperBound(0)] = assetNameTextBox.Text;
+            elections[elections.GetUpperBound(0)] = double.Parse(electionsTextBox.Text);
+            acts[acts.GetUpperBound(0)] = double.Parse(actsSubterfugeTextBox.Text);
+
+            //To store new data to VewState
+            ViewState["Assets"] = assets;
+            ViewState["Elections"] = elections;
+            ViewState["Acts"] = acts;
+
+            // To make label
+            resultLabel.Text = String.Format(
+                "Total Elections Rigged: {0}<br />" +
+                "Average Acts of Subterfuge per Asset: {1:N2}<br />" +
+                "(Last Asset you Added: {2})<br />",
+                elections.Sum(),
+                acts.Average(),
+                assets[assets.GetUpperBound(0)]
+                );
+
+            // To make TextBoxes blank
+            assetNameTextBox.Text = "";
+            electionsTextBox.Text = "";
+            actsSubterfugeTextBox.Text = "";
+
+            // For validate
+            /*
+            resultLabel.Text += assets.Length + "<br />";
+
+
+            for (int i=0; i<assets.Length; i++)
+            {
+                resultLabel.Text += assets[i] + " ";
+            }
+            */
+
+        }
+    }
+}
